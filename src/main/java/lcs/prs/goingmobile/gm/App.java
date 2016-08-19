9d483e5070890aba90570e2013784e09da5e9d28
kebeclibre <lcsperais@gmail.com>
2016-08-19 12:14:43 +0200

    entities created

---
+++ src/main/java/lcs/prs/goingmobile/gm/App.java
@@ -0,0 +1,13 @@
+package lcs.prs.goingmobile.gm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
